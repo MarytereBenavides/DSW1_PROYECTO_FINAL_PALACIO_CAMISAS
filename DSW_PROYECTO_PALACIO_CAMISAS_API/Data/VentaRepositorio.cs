c43a9a1009d823b095f24ca059137c17b4ea49a9
mbenavides <maryterebenavides@gmail.com>
2025-08-21 01:25:35 -0500

    fix ventas/get

--- DSW_PROYECTO_PALACIO_CAMISAS_API/Data/VentaRepositorio.cs
+++ DSW_PROYECTO_PALACIO_CAMISAS_API/Data/VentaRepositorio.cs
@@ -21,24 +21,53 @@ namespace DSW_PROYECTO_PALACIO_CAMISAS_API.Data
             using (var conexion = new SqlConnection(cadenaConexion))
             {
                 conexion.Open();
-                using (var comando = new SqlCommand("ListarVentas", conexion))
+                using (var comando = new SqlCommand("ListarVentasConDetalles", conexion))
                 {
                     comando.CommandType = System.Data.CommandType.StoredProcedure;
                     using (var reader = comando.ExecuteReader())
                     {
+                        var ventasDict = new Dictionary<int, Venta>();
+
                         while (reader.Read())
                         {
-                            listado.Add(new Venta()
+                            int idVenta = reader.GetInt32(0);
+
+                            if (!ventasDict.ContainsKey(idVenta))
                             {
-                                id_venta = reader.GetInt32(0),
-                                nombre_cliente = reader.GetString(1),
-                                dni_cliente = reader.GetString(2),
-                                tipo_pago = reader.GetString(3),
-                                fecha = reader.GetDateTime(4),
-                                precio_total = reader.GetDecimal(5),
-                                estado = reader.GetString(6)
-                            });
+                                ventasDict[idVenta] = new Venta()
+                                {
+                                    id_venta = idVenta,
+                                    nombre_cliente = reader.GetString(1),
+                                    dni_cliente = reader.GetString(2),
+                                    tipo_pago = reader.GetString(3),
+                                    fecha = reader.GetDateTime(4),
+                                    precio_total = reader.GetDecimal(5),
+                                    estado = reader.GetString(6),
+                                    detalles = new List<DetalleVenta>()
+                                };
+                            }
+
+                            if (!reader.IsDBNull(7))
+                            {
+                                var detalle = new DetalleVenta()
+                                {
+                                    id_venta = idVenta,
+                                    id_camisa = reader.GetInt32(7),
+                                    cantidad = reader.GetInt32(8),
+                                    precio = reader.GetDecimal(9),
+                                    estado = reader.GetString(10),
+                                    camisa_descripcion = reader.GetString(11),
+                                    camisa_color = reader.GetString(12),
+                                    camisa_talla = reader.GetString(13),
+                                    camisa_manga = reader.GetString(14),
+                                    marca_nombre = reader.GetString(15)
+                                };
+
+                                ventasDict[idVenta].detalles.Add(detalle);
+                            }
                         }
+
+                        listado = ventasDict.Values.ToList();
                     }
                 }
             }
@@ -51,30 +80,88 @@ namespace DSW_PROYECTO_PALACIO_CAMISAS_API.Data
             using (var conexion = new SqlConnection(cadenaConexion))
             {
                 conexion.Open();
-                using (var comando = new SqlCommand("ObtenerVentaPorID", conexion))
+                using (var comando = new SqlCommand("ObtenerVentaPorIDConDetalles", conexion))
                 {
                     comando.CommandType = System.Data.CommandType.StoredProcedure;
                     comando.Parameters.AddWithValue("@id_venta", id);
                     using (var reader = comando.ExecuteReader())
                     {
-                        if (reader != null && reader.HasRows)
+                        var detalles = new List<DetalleVenta>();
+
+                        while (reader.Read())
                         {
-                            reader.Read();
-                            venta = new Venta()
+                            if (venta == null)
                             {
-                                id_venta = reader.GetInt32(0),
-                                nombre_cliente = reader.GetString(1),
-                                dni_cliente = reader.GetString(2),
-                                tipo_pago = reader.GetString(3),
-                                fecha = reader.GetDateTime(4),
-                                precio_total = reader.GetDecimal(5),
-                                estado = reader.GetString(6)
-                            };
+                                venta = new Venta()
+                                {
+                                    id_venta = reader.GetInt32(0),
+                                    nombre_cliente = reader.GetString(1),
+                                    dni_cliente = reader.GetString(2),
+                                    tipo_pago = reader.GetString(3),
+                                    fecha = reader.GetDateTime(4),
+                                    precio_total = reader.GetDecimal(5),
+                                    estado = reader.GetString(6),
+                                    detalles = new List<DetalleVenta>()
+                                };
+                            }
+
+                            if (!reader.IsDBNull(7))
+                            {
+                                var detalle = new DetalleVenta()
+                                {
+                                    id_venta = id,
+                                    id_camisa = reader.GetInt32(7),
+                                    cantidad = reader.GetInt32(8),
+                                    precio = reader.GetDecimal(9),
+                                    estado = reader.GetString(10),
+                                    camisa_descripcion = reader.GetString(11),
+                                    camisa_color = reader.GetString(12),
+                                    camisa_talla = reader.GetString(13),
+                                    camisa_manga = reader.GetString(14),
+                                    marca_nombre = reader.GetString(15)
+                                };
+
+                                venta.detalles.Add(detalle);
+                            }
                         }
                     }
                 }
             }
             return venta;
+        }
+
+        public List<DetalleVenta> ObtenerDetallesPorVentaID(int idVenta)
+        {
+            var detalles = new List<DetalleVenta>();
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand("ObtenerDetallesPorVenta", conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+                    comando.Parameters.AddWithValue("@id_venta", idVenta);
+                    using (var reader = comando.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            detalles.Add(new DetalleVenta()
+                            {
+                                id_venta = idVenta,
+                                id_camisa = reader.GetInt32(0),
+                                cantidad = reader.GetInt32(1),
+                                precio = reader.GetDecimal(2),
+                                estado = reader.GetString(3),
+                                camisa_descripcion = reader.GetString(4),
+                                camisa_color = reader.GetString(5),
+                                camisa_talla = reader.GetString(6),
+                                camisa_manga = reader.GetString(7),
+                                marca_nombre = reader.GetString(8)
+                            });
+                        }
+                    }
+                }
+            }
+            return detalles;
         }
 
         public Venta Registrar(Venta venta, List<DetalleVenta> detalles)
@@ -89,7 +176,6 @@ namespace DSW_PROYECTO_PALACIO_CAMISAS_API.Data
                 {
                     try
                     {
-                        // Insertar venta
                         using (var comando = new SqlCommand("RegistrarVenta", conexion, transaccion))
                         {
                             comando.CommandType = System.Data.CommandType.StoredProcedure;
@@ -102,7 +188,6 @@ namespace DSW_PROYECTO_PALACIO_CAMISAS_API.Data
                             nuevoId = Convert.ToInt32(comando.ExecuteScalar());
                         }
 
-                        // Insertar detalles
                         foreach (var detalle in detalles)
                         {
                             using (var comando = new SqlCommand("RegistrarDetalleVenta", conexion, transaccion))
@@ -128,6 +213,48 @@ namespace DSW_PROYECTO_PALACIO_CAMISAS_API.Data
             }
             nuevaVenta = ObtenerPorID(nuevoId);
             return nuevaVenta;
+        }
+
+        public bool ActualizarEstado(int id, string estado)
+        {
+            var exito = false;
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (var transaccion = conexion.BeginTransaction())
+                {
+                    try
+                    {
+
+                        using (var comando = new SqlCommand("ActualizarEstadoVenta", conexion, transaccion))
+                        {
+                            comando.CommandType = System.Data.CommandType.StoredProcedure;
+                            comando.Parameters.AddWithValue("@id_venta", id);
+                            comando.Parameters.AddWithValue("@estado", estado);
+                            exito = comando.ExecuteNonQuery() > 0;
+                        }
+
+                        if (exito)
+                        {
+                            using (var comando = new SqlCommand("ActualizarEstadoDetalleVenta", conexion, transaccion))
+                            {
+                                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                                comando.Parameters.AddWithValue("@id_venta", id);
+                                comando.Parameters.AddWithValue("@estado", estado);
+                                comando.ExecuteNonQuery();
+                            }
+                        }
+
+                        transaccion.Commit();
+                    }
+                    catch
+                    {
+                        transaccion.Rollback();
+                        throw;
+                    }
+                }
+            }
+            return exito;
         }
     }
 }
