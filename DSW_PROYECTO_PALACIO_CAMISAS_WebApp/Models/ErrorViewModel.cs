ee7662dd01d5dd48de6f7455d0bf72c46fe95e81
mbenavides <maryterebenavides@gmail.com>
2025-08-20 22:56:49 -0500

    Primer fix - Proyecto Final Palacio Camisas

---
+++ DSW_PROYECTO_PALACIO_CAMISAS_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSW_PROYECTO_PALACIO_CAMISAS_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
